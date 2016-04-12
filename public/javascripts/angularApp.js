c6e7f7028debbfc3e818dc2b2a092aa8aeaf5c21
amscher <amscher@gmail.com>
2016-04-11 20:20:26 -0700

    first commit

---
+++ public/javascripts/angularApp.js
@@ -0,0 +1,199 @@
+var app = angular.module('cof-buyer', ['ui.router']);
+
+app.config([
+'$stateProvider',
+'$urlRouterProvider',
+function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+    .state('home', {
+      url: '/home',
+      templateUrl: '../partials/home.ejs',
+      controller: 'MainCtrl',
+      resolve: {
+        customerPromise: ['customers', function(customers) {
+          return customers.getAll();
+        }]
+      }
+    })
+  $urlRouterProvider.otherwise('home');
+}]);
+
+/* Posts Factory */
+app.factory('customers', ['$http', function($http){
+
+  var o = {
+    customers: []
+  };
+
+  o.getAll = function() {
+    return $http.get('/customers').success(function(data) {
+      angular.copy(data, o.customers);
+    });
+  }
+
+  o.getCards = function(customer) {
+    return $http.get('/customers/' + customer.id ).success(function(data) {
+      angular.copy(data, customer);
+    });
+  }
+
+  o.addCard = function(customer, nonce) {
+    return $http.post('/customers/' + customer.id + '/cards', {
+      "customer": customer,
+      "nonce": nonce
+    })
+    .success(function(data) {
+      o.getCards(customer);
+    });;
+  }
+
+  o.deleteCard = function(customer, card) {
+    return $http.delete('/customers/' + customer.id + '/cards/' + card.id)
+    .success(function(data) {
+      console.log(data);
+    });;
+  }
+
+  return o;
+}]);
+
+
+/* Main app controller */
+app.controller('MainCtrl', [
+  '$scope',
+  'customers',
+  function($scope, customers) {
+    $scope.customers = customers.customers;
+    $scope.filtered_customers = [];
+    $scope.selected_customer = null;
+    $scope.paymentCardNonce = null;
+    $scope.paymentForm = null;
+    $scope.show_payment_form = false;
+    $scope.add_card_success = false;
+    $scope.card_icons = {
+      "AMERICAN_EXPRESS" : "../images/Amex.png",
+      "VISA" : "../images/Visa.png",
+      "MASTER_CARD" : "../images/Mastercard.png",
+      "DISCOVER" : "../images/Discover.png",
+    };
+
+    var newPaymentForm = function() {
+      return new SqPaymentForm({
+        applicationId: 'sq0idp-0H4tmUZMLoKHfWYfWcFc2g', // <-- REQUIRED: Add Application ID
+        inputClass: 'form-control',
+        inputStyles: [
+          {
+            fontSize: '15px'
+          }
+        ],
+        cardNumber: {
+          elementId: 'sq-card-number',
+          placeholder: '•••• •••• •••• ••••'
+        },
+        cvv: {
+          elementId: 'sq-cvv',
+          placeholder: 'CVV'
+        },
+        expirationDate: {
+          elementId: 'sq-expiration-date',
+          placeholder: 'MM/YY'
+        },
+        postalCode: {
+          elementId: 'sq-postal-code'
+          placeholder: '94110'
+        },
+        callbacks: {
+          cardNonceResponseReceived: function(errors, nonce, cardData) {
+            if (errors) {
+              // handle errors
+              errors.forEach(function(error) { console.log(error.message); });
+            } else {
+              // handle nonce
+              $scope.paymentCardNonce = nonce;
+              customers.addCard($scope.selected_customer, nonce);
+              $scope.togglePaymentForm();
+              $scope.add_card_success = true;
+              console.log('Nonce received:');
+              console.log(nonce);
+            }
+          },
+          unsupportedBrowserDetected: function() {
+            // Alert the buyer that their browser is not supported
+          }
+        }
+      });
+    };
+
+    $scope.customersByEmail = function() {
+      console.log($scope.customers);
+      for(var i in $scope.customers) {
+        var customer = $scope.customers[i];
+        console.log(customer);
+        if(customer.email_address == $scope.email) $scope.filtered_customers.push(customer);
+      }
+      $scope.email = '';
+    }
+
+    $scope.togglePaymentForm = function() {
+      if ($scope.show_payment_form) {
+        $scope.show_payment_form = false;
+        $scope.paymentForm.destroy();
+        $scope.paymentForm = null;
+      } else {
+        $scope.paymentForm = new newPaymentForm();
+        $scope.paymentForm.build();
+        $scope.show_payment_form = true;
+        $scope.add_card_success = false;
+      }
+    }
+
+    $scope.toggleCustomer = function(customer) {
+      if ($scope.selected_customer != customer) {
+        $scope.selected_customer = customer;
+      } else {
+        $scope.selected_customer = null;
+      }
+    }
+
+    $scope.removeCard = function(index) {
+      var card = $scope.selected_customer.cards[index];
+      customers.deleteCard($scope.selected_customer, card)
+        .success(function(index) {
+          $scope.selected_customer.cards.splice(index, 1);
+        });
+    }
+
+
+      $scope.requestCardNonce = function() {
+        $scope.paymentForm.requestCardNonce();
+      }
+
+    }]);
+
+/* Auth factory */
+// app.factory('auth', ['$http', '$window', function($http, $window) {
+//   var auth = {};
+
+//   auth.saveToken = function(token) {
+//     $window.localStorage['flapper-news-token'] = token;
+//   }
+
+//   auth.getToken = function() {
+//     return $window.localStorage['flapper-news-token'];
+//   }
+
+//   auth.isLoggedIn = function() {
+//     var token = auth.getToken();
+
+//     if(token){
+//       var payload = JSON.parse($window.atob(token.split('.')[1]));
+
+//       return payload.exp > Date.now() / 1000;
+//     } else {
+//       return false;
+//     }
+//   }
+
+//   return auth;
+// }]);
---
+++ routes/index.js
@@ -0,0 +1,89 @@
+var express = require('express');
+var router = express.Router();
+
+var app = express();
+var config = require('.././config.json')[app.get('env')];
+
+var unirest = require('unirest');
+var base_url = "https://connect.squareup.com/v2";
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'CoF' , 'square_application_id': config.squareApplicationId });
+});
+
+/* SQ CUSTOMERS API CALLS */
+router.get('/customers', function(req, res, next) {
+
+  unirest.get(base_url + '/customers')
+  .headers({
+    'Authorization': 'Bearer ' + config.squareAccessToken,
+    'Accept': 'application/json',
+  })
+  .end(function (response) {
+    var customers = response.body.customers;
+    console.log(customers);
+    res.json(customers);
+  });
+});
+
+/* SQ CUSTOMERS API CALLS */
+router.get('/customers/:customer', function(req, res, next) {
+
+  unirest.get(base_url + '/customers/' + req.params.customer)
+  .headers({
+    'Authorization': 'Bearer ' + config.squareAccessToken,
+    'Accept': 'application/json',
+  })
+  .end(function (response) {
+    var customer = response.body.customer;
+    console.log(customer);
+    res.json(customer);
+  });
+});
+
+/* Add card to customer */
+router.post('/customers/:customer/cards', function(req, res, next) {
+  var request_params = req.body;
+
+  request_body = {
+    card_nonce: request_params.nonce,
+    cardholder_name: request_params.customer.give_name + ' ' + request_params.customer.family_name
+  }
+
+  unirest.post(base_url + '/customers/' + request_params.customer.id + "/cards")
+    .headers({
+      'Authorization': 'Bearer ' + config.squareAccessToken,
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    })
+    .send(request_body)
+    .end(function(response){
+      if (response.body.errors){
+        res.json({status: 400, errors: response.body.errors})
+      }else{
+        res.json({status: 200, card:response.body})
+      }
+    });
+});
+
+/* Delete card from customer */
+router.delete('/customers/:customer/cards/:card', function(req, res, next) {
+  console.log(req.params.customer);
+  unirest.delete(base_url + '/customers/' + req.params.customer + "/cards/" + req.params.card)
+    .headers({
+      'Authorization': 'Bearer ' + config.squareAccessToken,
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    })
+    .end(function(response){
+      if (response.body.errors){
+        res.json({status: 400, errors: response.body.errors})
+      }else{
+        res.json({status: 200})
+      }
+    });
+});
+
+module.exports = router;
